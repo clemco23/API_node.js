08c1dd62cc94114c4b2f7fa1e324a81e581bbf5b
clement <clementboscher6@gmail.com>
2025-09-19 14:25:27 +0200

    fix bug on updateUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,11 +37,19 @@ exports.createUser = async (req, res) => {
 
 exports.updateUser = async (req, res) => {
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        if (!user) {
+        const updates = { ...req.body };
+        if (updates.password) {
+            updates.password = await bcrypt.hash(updates.password, 10);
+        }
+        const updateUser = await User.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { new: true , runValidators: true}
+        ).select('-password');
+        if (!updateUser) {
             return res.status(404).json({ message: 'User not found' });
         }   
-        res.status(200).json({ user, message: 'User updated successfully' });
+        res.status(200).json({ updateUser, message: 'User updated successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -49,8 +57,8 @@ exports.updateUser = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);   
-        if (!user) {
+        const deleteUser = await User.findByIdAndDelete(req.params.id);   
+        if (!deleteUser) {
             return res.status(404).json({ message: 'User not found' });
         }
         res.status(200).json({ message: 'User deleted successfully' });
