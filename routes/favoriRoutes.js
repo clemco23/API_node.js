7f0da74e72bf743e4a1adcc618a8c3f3f548c7df
clement <clementboscher6@gmail.com>
2025-09-23 21:13:29 +0200

    update favoriRoutes

---
+++ routes/favoriRoutes.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+const router = express.Router();
+const favoriController = require('../controllers/favoriController');
+const verifyToken = require('../middlewares/verifyToken');
+
+//routes Privées
+router.get('/', verifyToken, favoriController.getAllFavoris);
+router.post('/', verifyToken, favoriController.addFavori);
